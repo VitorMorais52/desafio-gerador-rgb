55c34206bc5e1da5b2b16c30287fb5179ab63b97
Morais <vitormorais52@gmail.com>
2022-03-01 06:23:29 -0300

    create component structure, add styled components and implement global styling

--- rgb-generator-panel/src/App.tsx
+++ rgb-generator-panel/src/App.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+//components
+import Dashboard from "./components/pages/dashboard";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ rgb-generator-panel/src/components/pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+//styles
+import { Container } from "./styles";
+
+function Dashboard() {
+  return <Container></Container>;
+}
+
+export default Dashboard;
